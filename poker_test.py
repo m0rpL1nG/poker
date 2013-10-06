c92ef2120880b799b21e89d233c5c6b477d68b83
pethio <s6070003@kmitl.ac.th>
2013-10-06 19:55:56 +0700

    Clear poker_test.py code (again) for use tdd concept.
    Because of our not clear in step of tdd concept.
    We need to clear poker_test.py code for not making us confuse in step of coding.

--- poker_test.py
+++ poker_test.py
@@ -1,21 +1,17 @@
 import unittest
 import poker
 class Testpoker(unittest.TestCase):
-    '''Example unittest test methods for poker. [Return the hand rank of a hand]'''
+    """unittest test methods for poker. [Return the hand rank of a hand]"""
     def test_poker(self):
-    	'''Test test_poker with sf = ['JC', 'TC', '9C', '8C', '7C'] And sf2 = ['JS', 'TS', '9S', '8S', '7S'] '''
+    	"""Test poker with straight flush hand
+
+        ['JC', 'TC', '9C', '8C', '7C']
+        """
     	sf = ['JC', 'TC', '9C', '8C', '7C']
-    	actual = poker.poker(sf)
-        expected = ['JC', 'TC', '9C', '8C', '7C']
+    	actual = poker.poker([sf])
+        expected = 8
         self.assertEqual(actual, expected)
 
-        
-    def test_hand_rank(self):
-        '''Test hand_rank with sf = ['JC', 'TC', '9C', '8C', '7C']'''
-        sf = ['JC', 'TC', '9C', '8C', '7C']
-        actual = poker.hand_rank(sf)
-        expected = 8, 11 
-        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
