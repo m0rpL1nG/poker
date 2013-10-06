31c9aec4cdc91e36f9376221da473fd27a29127b
pethio <s6070003@kmitl.ac.th>
2013-10-06 19:54:42 +0700

    Clear poker.py code (again) for use tdd concept.
    Because of our not clear in step of tdd concept.
    We need to clear poker.py code for not making us confuse in step of coding.

--- poker.py
+++ poker.py
@@ -1,25 +1,7 @@
-def poker(hands):
+def poker(hand):
     """
-   ([hand, hand, ...])-> hands
-   Return the best hand from list of hands
-   """
-
-    return allmax(hands)
-def allmax(hands):
-    winhand = max(hands, key=hand_rank)
-    maxval = hand_rank(winhand)
-    result = []
-    for hand in hands:
-        if hand_rank(hand) == maxval:
-            result.append(hand)
-    return result
-def hand_rank(hand):
-    """
-    (hand)-> int
-
+    hand -> int
+    
     Return the hand rank of a hand
     """
-    
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    return hand_rank(hand)
